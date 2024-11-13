23b8b8493575ca6537a248da8eaad1e4b3e78e6e
Yulsen <apingspeedholic@gmail.com>
2024-11-13 16:23:23 +0700

    customizing components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
   return (
-    <div>
-      Homepage
+    <div className="">
+      <Button>Click</Button>
+      <Button variant={"primary"}>Click</Button>
+      <Button variant={"destructive"}>Click</Button>
+      <Button variant={"outline"}>Click</Button>
+      <Button variant={"secondary"}>Click</Button>
+      <Button variant={"ghost"}>Click</Button>
+      <Input />
     </div>
-  )
+  );
 }
